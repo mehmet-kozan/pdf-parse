0c694d2a48c8d289336d3d69ada73609b8080296
Mehmet Kozan <mehmet.kozan@live.com>
2025-10-06 12:06:40 +0300

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,31 +1,25 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	base: '/pdf-parse/', // Use relative paths for GitHub Pages compatibility
+	base: './',
 	test: {
 		environment: 'node',
 		globals: true,
 		threads: false,
 		exclude: ['**/node_modules/**', '**/dist/**', '**/_*', '**/_*.test.*', '**/_*.spec.*'],
-		reporters: [
-			'default',
-			[
-				'html',
-				{
-					outputFile: './reports/test/index.html',
-					summaryFile: '../coverage/coverage-summary.json',
-				},
-			],
-		],
+		reporters: ['default', 'html'],
+		outputFile: {
+			html: 'reports/test/index.html',
+		},
 		coverage: {
-			enabled: false,
+			enabled: true,
 			provider: 'v8',
 			include: ['src/**/*.ts'],
 			exclude: ['src/**/*.d.ts', 'src/types/**', 'src/index.cjs.ts'],
-			reporter: ['html', 'json-summary', 'json'],
-			reportsDirectory: './reports/coverage',
+			reporter: [['html', { subdir: 'coverage' }]],
+			reportsDirectory: 'reports',
 			all: true, // Include all files, even if not tested
-			clean: true, // Clean coverage directory before generating new report
+			clean: false, // Clean coverage directory before generating new report
 		},
 	},
 });
