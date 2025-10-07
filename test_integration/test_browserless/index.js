700dfb21321ed90000d6356381edf57c0b424882
Mehmet Kozan <mehmet.kozan@live.com>
2025-10-08 02:44:05 +0300

    Refactor browserless integration test formatting
    Standardizes code formatting in the browserless integration test by adding semicolons, fixing indentation, and removing unnecessary blank lines. No functional changes were made.

--- test_integration/test_browserless/index.js
+++ test_integration/test_browserless/index.js
@@ -1,26 +1,24 @@
-const { default: listen } = require('async-listen')
+const { default: listen } = require('async-listen');
 const { createServer } = require('node:http');
-const os = require('node:os')
+const os = require('node:os');
 const createBrowser = require('browserless');
 const pdf = require('pdf-parse');
 const local_pdf = require('../../dist/cjs/index.cjs');
 
+async function runServer(handler) {
+	const server = createServer(async (req, res) => {
+		await handler({ req, res });
+	});
 
-
-async function runServer(handler){
-  const server = createServer(async (req, res) => {
-    await handler({ req, res })
-  });
-
-  const url = await listen(server);
-  url.hostname = os.hostname();
-  const urlStr = url.toString();
-  return {server, url:urlStr}
+	const url = await listen(server);
+	url.hostname = os.hostname();
+	const urlStr = url.toString();
+	return { server, url: urlStr };
 }
 
 const getUrl = runServer(({ res }) => {
-    res.setHeader('Content-Type', 'text/html')
-    return res.end(`<!DOCTYPE html>
+	res.setHeader('Content-Type', 'text/html');
+	return res.end(`<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
@@ -30,35 +28,26 @@ const getUrl = runServer(({ res }) => {
     <body>
       <p>hello world</p>
     </body>
-    </html>`)
-  });
+    </html>`);
+});
 
 async function run() {
+	try {
+		const browser = createBrowser();
+		const browserless = await browser.createContext();
+		const { server, url } = await getUrl;
+		const buffer = await browserless.pdf(url);
+		server.close();
+		const data = await pdf(buffer);
+		const local_data = await local_pdf(buffer);
 
-    try{
-
-        const browser = createBrowser();
-        const browserless = await browser.createContext();
-        const {server, url} = await getUrl;
-        const buffer = await browserless.pdf(url);
-        server.close();
-        const data = await pdf(buffer);
-        const local_data = await local_pdf(buffer);
-
-        if(data.text.includes('hello world') && local_data.text.includes('hello world')){
-            process.exit(0);
-        }
-        else{
-            process.exit(1);
-        }
-
-    }
-    catch{
-        process.exit(1);
-    }
-
+		if (data.text.includes('hello world') && local_data.text.includes('hello world')) {
+			process.exit(0);
+		}
+		process.exit(1);
+	} catch {
+		process.exit(1);
+	}
 }
 
 run();
-
-
