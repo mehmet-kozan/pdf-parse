09acefedaba1f4926a88c5809587aeb2b8dc472d
Mehmet Kozan <mehmet.kozan@live.com>
2025-10-24 12:55:41 +0300

    v1.1.1 pushed.
    old pdf-parse version initilized.

--- src/InfoResult.ts
+++
@@ -1,7 +0,0 @@
-import type {Metadata} from "pdfjs-dist/types/src/display/metadata.js";
-
-export interface InfoResult {
-    total: number;
-    info: object | undefined;
-    metadata: Metadata | undefined;
-}
--- src/ParseOptions.ts
+++
@@ -1,9 +0,0 @@
-// check: https://rclayton.silvrback.com/easy-class-api-options-with-typescript-and-joi
-
-import type { DocumentInitParameters } from 'pdfjs-dist/types/src/display/api.js';
-
-export interface ParseOptions extends DocumentInitParameters {
-	partial?: boolean;
-	first?: number;
-	last?: number;
-}
