080fee60c9d7130c16ab4aed127191ad95da5cc2
Mehmet Kozan <mehmet.kozan@live.com>
2025-12-11 18:34:26 +0300

    Remove PDF parsing and utility modules
    Deleted all source files related to PDF parsing, geometry, image extraction, metadata, and worker utilities. This change removes the core implementation and supporting types/interfaces for PDF document processing.

--- src/pdf-parse/info/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './InfoData.js';
-export * from './InfoResult.js';
-export * from './MetaData.js';
-export * from './OutlineData.js';
-export * from './PageData.js';
-export * from './PermissionData.js';
